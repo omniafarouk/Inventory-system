2783f2755eec948a1d185310feaa3da7b5ee37ed
omniafarouk <145873718+omniafarouk@users.noreply.github.com>
2024-09-06 18:25:46 +0300

    files updated

--- src/main/java/ManagementSystem/Main.java
+++ src/main/java/ManagementSystem/Main.java
@@ -2,7 +2,7 @@ package ManagementSystem;
 
 import frontend.InventorySystem;
 
-public class Assignment4 {
+public class Main {
 
     public static void main(String[] args) {
        InventorySystem system = new InventorySystem();
